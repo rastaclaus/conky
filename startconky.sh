3582803808da90047c8516d875692fee52ab62df
Шестопалов Артём Алексеевич <a.a.shestopalov@sozvezdie.su>
2019-04-08 11:44:59 +0300

    move calendar and add news

--- startconky.sh
+++ startconky.sh
@@ -3,4 +3,5 @@ conky -c ~/.conky/rings & # the main conky with rings
 conky -c ~/.conky/cpu &
 conky -c ~/.conky/mem &
 conky -c ~/.conky/notes &
+conky -c ~/.conky/news &
 conky -c ~/.conky/calendar &
