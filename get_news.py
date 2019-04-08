81c3e23725ae53febd592a128f25f28cf8510b0b
Шестопалов Артём Алексеевич <a.a.shestopalov@sozvezdie.su>
2019-04-08 11:46:23 +0300

    add get_news.py

---
+++ get_news.py
@@ -0,0 +1,14 @@
+import textwrap
+import requests
+from pprint import pprint
+from bs4 import BeautifulSoup
+
+URL = "https://news.yandex.ru/index.rss"
+xml = requests.get(URL).text
+soup = BeautifulSoup(xml, 'xml')
+
+news = [item.title.string for item in soup.find_all('item')]
+for news_item in news[:5]:
+    for line in textwrap.wrap(news_item):
+        print(line)
+    print()
