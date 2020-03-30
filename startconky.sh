e05e49982a3fabf42ce44c505179d5e03ce7d4bd
tema rastaclaus <tema.rastaclaus@gmail.com>
2020-03-31 01:11:52 +0300

    fix diff

--- startconky.sh
+++ startconky.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+sudo ps -aux | grep conky | grep -v startconky | cut -d' ' -f5 | xargs kill -KILL;
+
 conky -c ~/.conky/rings & # the main conky with rings
 conky -c ~/.conky/cpu &
 conky -c ~/.conky/mem &
