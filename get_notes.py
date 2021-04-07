004c92ef9cec5bca9df54627b458778dd4432245
Artem Shestopalov <tema.rastaclaus@gmail.com>
2021-04-08 01:23:54 +0300

    joplin connection

--- get_notes.py
+++ get_notes.py
@@ -1,18 +1,44 @@
-import os
-from pathlib import Path
-import textwrap
+import subprocess as sp
+import json
+from pprint import pformat
+from textwrap import wrap
+
+NOTES_COMMAND = ['joplin', 'ls', '-l', '-t', 'n', '--format', 'json']
 
 
-HOME = os.getenv('HOME')
-NOTES_PATH = Path(HOME) / 'notes.txt'
+def get_note_ids():
+    res = sp.run(NOTES_COMMAND, stdout=sp.PIPE, encoding='utf-8')
+    data = json.loads(res.stdout)
+    for entry in data:
+        yield entry['id']
 
 
-def main(notes_path=NOTES_PATH):
-    with open(notes_path) as notes_file:
-        for line in notes_file:
-            for wrapped in textwrap.wrap(line, 40):
-                print(wrapped)
-            print()
+def print_note_text(note_text):
+    lines = note_text.split('\n')
+    header = lines[0]
+    if len(lines) > 2:
+        header += ':'
+    print(header)
+    text = ' '.join(lines[1:])
+    for line in wrap(text, 38):
+        print(line)
+
+
+def get_note_text(note_id):
+    command = ['joplin', 'cat', note_id, '--format', 'json']
+    res = sp.run(command, stdout=sp.PIPE, stderr=sp.PIPE, encoding='utf-8')
+    return res.stdout
+
+
+def print_note(note_id):
+    note_text = get_note_text(note_id)
+    print_note_text(note_text)
+
+
+def main():
+    for note_id in get_note_ids():
+        print_note(note_id)
+        print()
 
 
 if __name__ == '__main__':
